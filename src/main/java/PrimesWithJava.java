9f3a6f73db2175dd7934eae2dbc504d9c5768a9f
Javier Seixas <javier.seixas@scmspain.com>
2017-09-14 19:02:27 +0200

    Increases search limit

--- src/main/java/PrimesWithJava.java
+++ src/main/java/PrimesWithJava.java
@@ -1,8 +1,11 @@
 import java.util.stream.IntStream;
 
 class PrimesWithJava {
+
+  public static final int SEARCH_LIMIT = 2000;
+
   public static void main(String args[]) {
-    IntStream.rangeClosed(2, 100)
+    IntStream.rangeClosed(2, SEARCH_LIMIT)
             .filter(i -> IntStream.rangeClosed(2, (int)Math.sqrt(i))
                     .allMatch(j -> i%j != 0))
             .filter(prime -> {
