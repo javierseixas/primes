0d035ce84662a84bd71cfa62df3cba61cc6183be
Javier Seixas <javier.seixas@scmspain.com>
2017-09-14 19:01:51 +0200

    Ads filter for getting only ended with 7

--- src/main/java/PrimesWithJava.java
+++ src/main/java/PrimesWithJava.java
@@ -5,6 +5,11 @@ class PrimesWithJava {
     IntStream.rangeClosed(2, 100)
             .filter(i -> IntStream.rangeClosed(2, (int)Math.sqrt(i))
                     .allMatch(j -> i%j != 0))
+            .filter(prime -> {
+              String primeToString = String.valueOf(prime);
+              String lastNumber = primeToString.substring(primeToString.length() - 1);
+              return lastNumber.equals("7");
+            })
             .forEach(n -> {
               System.out.println(n);
             });
