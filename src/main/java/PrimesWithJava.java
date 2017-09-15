5cb9f64ba9331a1151f83367bc004377794b9e7d
Javier Seixas <javier.seixas@scmspain.com>
2017-09-15 10:27:43 +0200

    Collects primes in list for format printing

--- src/main/java/PrimesWithJava.java
+++ src/main/java/PrimesWithJava.java
@@ -1,20 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 class PrimesWithJava {
 
   public static final int SEARCH_LIMIT = 2000;
   public static final int RESULT_LIMIT = 60;
+  private static List<Integer> primes = new ArrayList<>(RESULT_LIMIT);
 
   public static void main(String args[]) {
 
     IntStream.rangeClosed(2, SEARCH_LIMIT)
-            .filter(i -> IntStream.rangeClosed(2, (int)Math.sqrt(i))
-                    .allMatch(j -> i%j != 0))
+            .filter(i -> IntStream.rangeClosed(2, (int) Math.sqrt(i))
+                    .allMatch(j -> i % j != 0))
             .filter(PrimesWithJava::endsWith7)
             .limit(RESULT_LIMIT)
-            .forEach(n -> {
-              System.out.println(n);
-            });
+            .forEach(primes::add);
+
+    System.out.println(primes);
   }
 
   private static boolean endsWith7(int prime) {
